8a75ddea0db7299a0237fa52e9d70864e3bc3405
qumarkq <top1686887@gmail.com>
2024-05-01 15:27:00 +0800

    change lab4

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import CGU_Login from './cgu_login';
+function App() {
+  return (
+    <div className="App">
+      { CGU_Login()}
+    </div>
+  );
+}
+
+export default App;
