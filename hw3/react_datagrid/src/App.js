e558883ed3ab42afe355b6a6f44bb18c53c198ce
qumarkq <top1686887@gmail.com>
2024-05-07 17:39:27 +0800

    hw3

---
+++ hw3/react_datagrid/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HW3 from './HW3.js';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ height: 0, marginBottom: 15 }}>景點觀光展覽資訊</h1>
+      {HW3()}
+    </div>
+  );
+}
+
+export default App;
