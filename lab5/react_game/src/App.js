05c268d74a31d75087b38d68167288bc4560c157
qumarkq <top1686887@gmail.com>
2024-05-08 16:24:47 +0800

    add lab5

--- lab5/react_game/src/App.js
+++ lab5/react_game/src/App.js
@@ -1,10 +1,8 @@
-//import logo from './logo.svg';
 import './App.css';
-import CGU_Login from './cgu_login';
+
 function App() {
   return (
     <div className="App">
-      { CGU_Login()}
     </div>
   );
 }
---
+++ lab5/react_game/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Game from './Game';
+import React from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div className='game'>
+    <Game />
+  </div>
+);
